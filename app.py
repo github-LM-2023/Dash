6018f78a27f58225393155248aeddfb584f3bc92
github-LM-2023 <128680956+github-LM-2023@users.noreply.github.com>
2023-09-03 01:13:52 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import dash
+import dash_table
+import pandas as pd
+
+# 读取本地Excel文件
+df = pd.read_excel(r"C:\Users\LM\Desktop\建筑储能-清华\清华车辆代码\网页\flight.xlsx",index_col=0)
+
+# 创建Dash应用
+app = dash.Dash(__name__)
+
+# 设置表格布局
+app.layout = dash_table.DataTable(
+    data=df.to_dict('records'),
+    columns=[{'name': col, 'id': col} for col in df.columns],
+    page_size=10,  # 每页显示10行数据
+    style_table={'height': '300px', 'overflowY': 'scroll'},
+)
+
+if __name__ == '__main__':
+    app.run_server(debug=False)
